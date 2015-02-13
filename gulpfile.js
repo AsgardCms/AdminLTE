3dd29ad0375d4cc12896cafd191ffc2be347cc14
Nicolas Widart <n.widart@gmail.com>
2015-02-13 11:13:55 +0100

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function (mix) {
+
+    /**
+     * Compile less
+     */
+    mix.less([
+        "main.less"
+    ]);
+
+    /**
+     * Concat scripts
+     */
+    mix.scripts([
+        '/vendor/jquery/dist/jquery.js',
+        '/vendor/bootstrap/dist/js/bootstrap.min.js',
+        '/vendor/prismjs/prism.js',
+        '/js/bootswatch.js'
+    ], null, 'resources');
+
+    /**
+     * Copy Bootstrap fonts
+     */
+    mix.copy(
+        'resources/vendor/bootstrap/fonts',
+        'assets/fonts'
+    );
+
+    /**
+     * Copy Fontawesome fonts
+     */
+    mix.copy(
+        'resources/vendor/font-awesome/fonts',
+        'assets/fonts'
+    );
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+# AsgardCMS Demo theme
+
+## Installation
+
+Add package to your `require` key in the `composer.json` file:
+
+``` json
+"asgardcms/adminlte-theme": "dev-master"
+```
+
+Followed by a composer update.
+
+You'll find the theme in the `Themes/` directory.
