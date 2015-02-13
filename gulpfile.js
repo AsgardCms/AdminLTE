855e71a6ddc87c609d707bcbcc71e7ec05aa84f9
Nicolas Widart <n.widart@gmail.com>
2015-02-13 15:38:36 +0100

    Change import order

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-var elixir = require('laravel-elixir');
 var gulp = require("gulp");
 var shell = require('gulp-shell');
+var elixir = require('laravel-elixir');
 
 elixir.extend("stylistPublish", function() {
     gulp.task("stylistPublish", function() {
