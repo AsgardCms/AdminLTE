c430b22a15b772fc5ffdf654430397d33c4d78c5
Nicolas Widart <n.widart@gmail.com>
2015-03-18 21:10:08 +0100

    New footer partial

---
+++ views/partials/footer.blade.php
@@ -0,0 +1,22 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0 beta
+    </div>
+    @yield('footer')
+</footer>
+
+<div class="modal fade" id="keyboardShortcutsModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">
+                    {{ trans('core::core.general.available keyboard shortcuts') }}
+                </h4>
+            </div>
+            <div class="modal-body">
+                @yield('shortcuts')
+            </div>
+        </div>
+    </div>
+</div>
