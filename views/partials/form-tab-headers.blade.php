0a65f3589664c8ab9422de1e492b2d55c97a0000
Nicolas Widart <n.widart@gmail.com>
2015-04-09 17:39:52 +0200

    Check for form errors on the current locale

--- views/partials/form-tab-headers.blade.php
+++ views/partials/form-tab-headers.blade.php
@@ -1,8 +1,19 @@
+<style>
+    li.error {
+        border-top-color: #dd4b39 !important;
+    }
+</style>
 <ul class="nav nav-tabs">
 <?php $i = 0; ?>
 <?php foreach (LaravelLocalization::getSupportedLocales() as $locale => $language): ?>
     <?php $i++; ?>
-    <li class="{{ App::getLocale() == $locale ? 'active' : '' }}">
+    <?php $class = ''; ?>
+    <?php if (isset($fields)): ?>
+        <?php foreach($fields as $field): ?>
+            <?php $class = $errors->has("{$locale}.{$field}") ? 'error' : '' ?>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    <li class="{{ App::getLocale() == $locale ? 'active' : '' }} {{ $class }}">
         <a href="#tab_{{ $i }}" data-toggle="tab">{{ trans('core::core.tab.'. strtolower($language['name'])) }}</a>
     </li>
 <?php endforeach; ?>
