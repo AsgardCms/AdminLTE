7894aac7a21cabc2d101ccf3b9b292b107a372fd
Nicolas Widart <n.widart@gmail.com>
2015-08-05 21:00:45 +0200

    Adding missing space

--- views/partials/top-nav.blade.php
+++ views/partials/top-nav.blade.php
@@ -10,7 +10,7 @@
     <div class="navbar-custom-menu">
 
         <ul class="nav navbar-nav">
-            <li><a href="{{ URL::to('/') }}" target="_blank"><i class="fa fa-eye"></i>{{trans('core::core.general.view website')}}</a></li>
+            <li><a href="{{ URL::to('/') }}" target="_blank"><i class="fa fa-eye"></i> {{trans('core::core.general.view website')}}</a></li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                     <i class="fa fa-flag"></i>
