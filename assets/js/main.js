ca803b1d865a4c35caa335d38d85cd5ee2389640
Micheal Mand <micheal@kmdwebdesigns.com>
2015-10-31 15:41:54 -0500

    Add slugify to main.js from app.js (which is not loaded/used)
    The file {resources,assets}/js/app.js does not appear to be loaded
    by the Asset Manager. I tried to compare that file to the vendor-
    supplied copy from AdminLTE:
    
    public/themes/adminlte/vendor/admin-lte/dist/js/app.js
    
    but the files are way to different to really tell what else might
    be missing.
    
    Signed-off-by: Micheal Mand <micheal@kmdwebdesigns.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     $("form").sisyphus({locationBased: true});
+    if ($('.slugify').length) {
+        $('.slugify').slug({
+            slug: 'slug',       // class of input / span that contains the generated slug
+            hide: false        // hide the text input, true by default
+        });
+    }
 });
