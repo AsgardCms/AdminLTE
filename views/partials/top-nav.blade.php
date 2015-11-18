99ea24a1f2192716758f143870ebc52975428a50
Nicolas Widart <n.widart@gmail.com>
2015-11-18 19:47:52 +0100

    Extract the notifications zone to partial

--- views/partials/top-nav.blade.php
+++ views/partials/top-nav.blade.php
@@ -8,52 +8,10 @@
         <span class="icon-bar"></span>
     </a>
 
-    <style>
-        .notificationsCounter {
-            animation-duration: .5s;
-        }
-        .notificationIcon {
-            font-size: 30px;
-        }
-    </style>
     <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
             <?php if (is_module_enabled('Notification')): ?>
-            <li class="dropdown messages-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
-                    <i class="fa fa-bell-o"></i>
-                    <span class="label label-success notificationsCounter animated">{{ $notifications->count() }}</span>
-                </a>
-                <ul class="dropdown-menu">
-                    <li>
-                        <div class="slimScrollDiv">
-                            <ul class="menu notifications-list">
-                                <?php if ($notifications->count() === 0): ?>
-                                    <li class="noNotifications">
-                                        <a href="">
-                                            No notifications
-                                        </a>
-                                    </li>
-                                <?php endif; ?>
-                                <?php foreach ($notifications as $notification): ?>
-                                <li>
-                                    <a href="{{ $notification->link }}">
-                                        <div class="pull-left notificationIcon">
-                                            <i class="{{ $notification->icon_class }}"></i>
-                                        </div>
-                                        <h4>
-                                            {{ $notification->title }}
-                                            <small><i class="fa fa-clock-o"></i> {{ $notification->time_ago }}</small>
-                                        </h4>
-                                        <p>{{ $notification->message }}</p>
-                                    </a>
-                                </li>
-                                <?php endforeach; ?>
-                            </ul>
-                        </div>
-                    </li>
-                </ul>
-            </li>
+            @include('notification::partials.notifications')
             <?php endif; ?>
             <li><a href="{{ URL::to('/') }}" target="_blank"><i class="fa fa-eye"></i> {{trans('core::core.general.view website')}}</a></li>
             <li class="dropdown">
